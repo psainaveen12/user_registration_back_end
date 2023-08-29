d0b1d75d87b70b6c4f6173e15cd79f69eaf1b424
psainaveen12 <puttisainaveen@gmail.com>
2023-08-29 11:27:59 -0400

    Added PostgreSQL DB to properties

--- src/main/java/com/Fullstackwebapp/Fullstackwebapp1/model/User.java
+++ src/main/java/com/Fullstackwebapp/Fullstackwebapp1/model/User.java
@@ -3,10 +3,12 @@ package com.Fullstackwebapp.Fullstackwebapp1.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "user_reg_tbl1")
 public class User {
 
     @Id
