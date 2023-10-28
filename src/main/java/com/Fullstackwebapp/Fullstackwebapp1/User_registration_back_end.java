c6f5a121ff371dac5d8d9c20bb2eed9b4be8793b
psainaveen12 <puttisainaveen@gmail.com>
2023-10-28 12:16:16 -0400

    Updated

--- src/main/java/com/Fullstackwebapp/Fullstackwebapp1/User_registration_back_end.java
+++ src/main/java/com/Fullstackwebapp/Fullstackwebapp1/User_registration_back_end.java
@@ -2,7 +2,7 @@ package com.Fullstackwebapp.Fullstackwebapp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
 public class User_registration_back_end {
@@ -10,5 +10,4 @@ public class User_registration_back_end {
 	public static void main(String[] args) {
 		SpringApplication.run(User_registration_back_end.class, args);
 	}
-
 }
